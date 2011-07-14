4b64af5443c2eab0b8116ea6c83c8a5c898538f8
Wyatt Lee Baldwin <baldwinw@trimet.org>
2011-07-14 12:13:54 -0700

    Cleaned up & improved `setup()`.
    - Added `long_description` option (= contents of README)
    - Added `classifiers` option, mainly so that supported Python versions were noted

--- setup.py
+++ setup.py
@@ -2,26 +2,35 @@ from setuptools import setup, find_packages
 
 version = '0.1dev'
 
-setup(name='gtfsdb',
-      version=version,
-      description="GTFS Database",
-      keywords='GTFS',
-      author='Mike Gilligan',
-      author_email='gilligam@trimet.org',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=(
-        'GeoAlchemy>=0.4.1',
-        'SQLAlchemy>=0.6.1',
-      ),
-      extras_require = {
-        'oracle': ['cx_Oracle>=5.0.3'],
-        'postgres': ['psycopg2>=2.0.14'],
-      },
-      entry_points={
-        'console_scripts': [
-            'gtfsdb-load = gtfsdb.scripts.load:main'
-        ]
-      }
+setup(
+    name='gtfsdb',
+    version=version,
+    description='GTFS Database',
+    long_description=open('README').read(),
+    keywords='GTFS',
+    author='Mike Gilligan',
+    author_email='gilligam@trimet.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=(
+      'GeoAlchemy>=0.4.1',
+      'SQLAlchemy>=0.6.1',
+    ),
+    extras_require = {
+      'oracle': ['cx_Oracle>=5.0.3'],
+      'postgres': ['psycopg2>=2.0.14'],
+    },
+    entry_points={
+      'console_scripts': [
+          'gtfsdb-load = gtfsdb.scripts.load:main'
+      ]
+    },
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
 )
