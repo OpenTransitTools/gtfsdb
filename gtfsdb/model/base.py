1a38d9e550716679f245ec291ab09539376feabc
Frank Purcell <purcellf@trimet.org>
2012-12-05 17:56:19 -0800

    Added a to_dict to the base class, to help with marshalling via jsonsimple

--- gtfsdb/model/base.py
+++ gtfsdb/model/base.py
@@ -21,6 +21,25 @@ class _Base(object):
         c = cls(**clean_dict)
         return c
 
+    def to_dict(self):
+        ''' convert a SQLAlchemy object into a dict that is serializable to JSON
+        ''' 
+        ret_val = self.__dict__.copy()
+
+        ''' not crazy about this hack, but ... 
+            the __dict__ on a SQLAlchemy object contains hidden crap that we delete from the class dict
+        '''
+        if set(['_sa_instance_state']).issubset(ret_val):
+            del ret_val['_sa_instance_state']
+
+        ''' we're using 'created' as the date parameter, so convert values to strings
+            TODO: better would be to detect date & datetime objects, and convert those...
+        '''
+        if set(['created']).issubset(ret_val):
+            ret_val['created'] = ret_val['created'].__str__();
+
+        return ret_val 
+
     @classmethod
     def get_filename(cls):
         return '%s.txt' %(cls.__tablename__)
