040c3627dbab515af08061c600d9de545a791d20
Mike Gilligan <gilligam@trimet.org>
2013-10-30 15:58:41 -0700

    Back to development: 0.1.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info[:2] <= (2, 6):
 
 setup(
     name='gtfsdb',
-    version='0.1.2',
+    version='0.1.3dev',
     description='GTFS Database',
     long_description=open('README').read(),
     keywords='GTFS',
