a4a65bee2509d079b1174c4ad3592dd2fce2bdf3
Mike Gilligan <gilligam@trimet.org>
2014-03-18 14:11:15 -0700

    Preparing release 0.1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info[:2] <= (2, 6):
 
 setup(
     name='gtfsdb',
-    version='0.1.4dev',
+    version='0.1.4',
     description='GTFS Database',
     long_description=open('README').read(),
     keywords='GTFS',
