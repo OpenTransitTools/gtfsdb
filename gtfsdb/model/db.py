cf7550f1936db6109464688aff55d8dc43143d56
Mike Gilligan <gilligam@trimet.org>
2014-03-18 14:09:29 -0700

    Added `Database.sorted_classes` property to return ORMs in the correct order based on foreign key constraints

--- gtfsdb/model/db.py
+++ gtfsdb/model/db.py
@@ -7,6 +7,12 @@ from gtfsdb import config
 class Database(object):
 
     def __init__(self, **kwargs):
+        '''
+        keyword arguments:
+            url: SQLAlchemy database url
+            schema: database schema name
+            is_geospatial: if database supports geo functions
+        '''
         self.url = kwargs.get('url', config.DEFAULT_DATABASE_URL)
         self.schema = kwargs.get('schema', config.DEFAULT_SCHEMA)
         self.is_geospatial = kwargs.get('is_geospatial',
@@ -59,6 +65,16 @@ class Database(object):
         self._schema = val
         for cls in self.classes:
             cls.__table__.schema = val
+
+    @property
+    def sorted_classes(self):
+        classes = []
+        for t in self.metadata.sorted_tables:
+            cls = next((c for c in self.classes
+                        if c.__table__ == t), None)
+            if cls:
+                classes.append(cls)
+        return classes
 
     @property
     def url(self):
