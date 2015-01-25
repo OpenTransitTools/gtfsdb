3fd44db073d6b3b20f616e25d3e29ea5ecaf5f91
Frank Purcell <fxpurcell@gmail.com>
2015-01-24 19:48:03 -0800

    fixes build on systems w/out oracle

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ import sys
 extras_require = dict(
     dev=[],
     oracle=['cx_oracle>=5.1'],
-    postgresql=['psycopg2>=2.4.2'],
 )
 
-install_requires = ['sqlalchemy == 0.8.6', 'geoalchemy>=0.7.2']
+install_requires = ['sqlalchemy == 0.8.6', 'geoalchemy>=0.7.2', 'psycopg2>=2.4.2']
 if sys.version_info[:2] <= (2, 6):
     install_requires.append('argparse>=1.2.1')
     extras_require['dev'].append('unittest2')
