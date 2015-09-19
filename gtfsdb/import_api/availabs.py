05ca698b0c7bcb3c447bcd16c9ed3f09feaea760
Ryan G. Hunter <ryan@cens.io>
2015-09-19 16:19:52 -0400

    fixed a bug in availlabs api

--- gtfsdb/import_api/availabs.py
+++ gtfsdb/import_api/availabs.py
@@ -6,6 +6,7 @@ from gtfsdb.import_api.common import get_json_url
 def get_agencies():
     return get_json_url('http://api.availabs.org/gtfs/agency/')
 
+
 def get_routes(agency):
-    return get_json_url('http://api.availabs.org/gtfs/agency/{}/routes'.format(agency['dataexchange_id'])
+    return get_json_url('http://api.availabs.org/gtfs/agency/{}/routes'.format(agency['dataexchange_id']))
 
