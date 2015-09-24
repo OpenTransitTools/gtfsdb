8cc21b1e0c8ab72aa90a66813127dfa0346939ae
Ryan G. Hunter <ryan@cens.io>
2015-09-24 14:05:29 -0400

    load geoms

--- gtfsdb/api.py
+++ gtfsdb/api.py
@@ -1,37 +1,9 @@
 from gtfsdb import Database, GTFS
-from gtfsdb.model.feed_info import DataexchangeInfo
 import logging
-import datetime
 import traceback
 import sys
 
 log = logging.getLogger(__name__)
-
-'''
-{
-    'dataexchange_id': "",
-    'file_url': "URL",
-    'file_name': "Filename",
-    'file_checksum' : "MD5Has"
-    'date_added': 1213154234.0
-}
-'''
-
-
-def database_load_versioned(source_meta, db_url, force=False):
-    db = Database(url=db_url, is_geospatial=True)
-    exchange_record = DataexchangeInfo(
-        agency_id=source_meta['dataexchange_id'], **source_meta)
-    if force or DataexchangeInfo.overwrite(db, exchange_record):
-        exchange_record.completed=database_load(source_meta['file_url'], db_url=db_url)
-        exchange_record.completed_on = datetime.datetime.utcnow()
-        session = db.get_session()
-        session.merge(exchange_record)
-        session.commit()
-    else:
-        rec = db.session.query(DataexchangeInfo).get(source_meta['dataexchange_id'])
-        logging.debug("Feed '{}' already uploaded on: {}".format(rec.dataexchange_id, rec.completed_on))
-
 
 def database_load(filename, db_url):
     try:
@@ -44,3 +16,8 @@ def database_load(filename, db_url):
         traceback.print_exc(file=sys.stdout)
         log.error('Error processing: {} Message: {}'.format(filename,e))
         return False
+
+def create_shapes_geoms(db_url):
+    db = Database(url=db_url, is_geospatial=True)
+    gtfs = GTFS()
+    gtfs.load_derived(db)
