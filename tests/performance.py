9f82976ba8f595647d54668728d969592f440eb8
Ryan G. Hunter <ryan@cens.io>
2015-09-26 22:06:55 -0400

    updated performance test

--- tests/performance.py
+++ tests/performance.py
@@ -11,7 +11,7 @@ def run_import(url):
         db = Database(url=postgresql.url())
         db.create()
         root_dir = os.path.dirname(__file__)
-        database_load(os.path.join(root_dir, 'data/sample-feed.zip'), db_url=url)
+        database_load(os.path.join(root_dir, 'data/performance-dataset.zip'), db_url=url)
         db.drop_all()
 
 with testing.postgresql.Postgresql() as postgresql:
@@ -20,7 +20,7 @@ with testing.postgresql.Postgresql() as postgresql:
         db.engine.execute('create extension postgis_topology;')
 
         start_time = time.time()
-        run_x = 50
+        run_x = 1
 
         for _ in range(run_x):
                 run_import(postgresql.url())
