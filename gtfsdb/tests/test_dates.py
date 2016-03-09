a12dc7a274230608cd4251b05d0fac5c098ef3bc
Frank Purcell <fxpurcell@gmail.com>
2016-03-09 14:52:28 -0800

    fix up test so it can be run indivdually

--- gtfsdb/tests/test_dates.py
+++ gtfsdb/tests/test_dates.py
@@ -1,9 +1,11 @@
+import os
+import sys
+import shutil
+import tempfile
+import datetime
 import logging
 log = logging.getLogger(__name__)
 
-import datetime
-import os
-import tempfile
 from pkg_resources import resource_filename
 try:
     import unittest2 as unittest
@@ -16,9 +18,10 @@ from gtfsdb.api import database_load
 class BasicModelTests(object):
     path = resource_filename('gtfsdb', 'tests')
     gtfs_file = 'file:///{0}'.format(os.path.join(path, 'multi-date-feed.zip'))
-    url = 'sqlite:///{0}'.format(tempfile.mkstemp()[1])
-    log.debug(url)
+    db_file = tempfile.mkstemp()[1]
+    url = 'sqlite:///{0}'.format(db_file)
     db = database_load(gtfs_file, url=url)
+    log.debug(db_file)
 
 class TestRouteStop(unittest.TestCase, BasicModelTests):
     model = RouteStop
@@ -30,3 +33,12 @@ class TestRouteStop(unittest.TestCase, BasicModelTests):
         for s in stops:
             self.assertTrue("good, I see active stop id: {0}".format(s.stop_id))
 
+    def __init__(self): pass # uncomment for debugging via main below
+
+def main(argv):
+    t = TestRouteStop()
+    t.test_active_list()
+    shutil.copyfile(t.db_file, "gtfs.db")
+
+if __name__ == "__main__":
+    main(sys.argv)
