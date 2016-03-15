cd6a51d77a71999430f444af435ac3c2b7d0f50c
Frank Purcell <fxpurcell@gmail.com>
2016-03-14 18:12:45 -0700

    more helper routines, more tests ... unique routes for a given stop

--- gtfsdb/tests/test_dates.py
+++ gtfsdb/tests/test_dates.py
@@ -40,10 +40,17 @@ class TestRouteStop(unittest.TestCase, BasicModelTests):
         rs = RouteStop.active_stops(self.db.session, route_id="NEW", direction_id="0", date=date)
         self.assertTrue(len(rs) == 0)
 
-    def test_old_stops(self):
+    def test_via_stops(self):
         #date = datetime.date(2015, 6, 6)
         rs = RouteStop.query_by_stop(self.db.session, stop_id="OLD")
         self.assertTrue(len(rs) >= 2)
+
+        routes = RouteStop.unique_routes_at_stop(self.db.session, stop_id="OLD")
+        self.assertTrue(len(routes) == 1)
+
+    def test_routes_via_stop(self):
+        routes = RouteStop.unique_routes_at_stop(self.db.session, stop_id="STAGECOACH", route_name_filter=True)
+        self.assertTrue(len(routes) == 3)
 
     def test_new_routes(self):
         date = datetime.date(2016, 6, 6)
@@ -72,7 +79,8 @@ def main(argv):
     shutil.copyfile(TestRouteStop.db_file, "gtfs.db")
     t = TestRouteStop()
     #import pdb; pdb.set_trace()
-    t.test_active_list()
+    #t.test_active_list()
+    t.test_old_stops()
 
 if __name__ == "__main__":
     main(sys.argv)
