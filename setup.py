bea0dc68b26cab5dcff34a7946c01c20be4a0a74
Purcell, Frank <purcellf@trimet.org>
2017-11-01 13:18:06 -0700

    fixes a buildout issue due to dev being in version name; reformatting dos NL/CR crap

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ if sys.version_info[:2] <= (2, 6):
 
 setup(
     name='gtfsdb',
-    version='0.1.6dev',
+    version='0.1.7',
     description='GTFS Database',
     long_description=open('README.rst').read(),
     keywords='GTFS',
-    author='Mike Gilligan',
-    author_email='gilligam@trimet.org',
+    author="Open Transit Tools",
+    author_email="info@opentransittools.org",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
