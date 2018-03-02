36c8bf1f3ce951bee9c02bdb7a9addabd1be5a97
Frank Purcell <fxpurcell@gmail.com>
2018-03-01 16:01:08 -0800

    add intersects routines

--- gtfsdb/util_geo.py
+++ gtfsdb/util_geo.py
@@ -20,3 +20,19 @@ class ST_MakePolygon(GenericFunction):
 class ST_Collect(GenericFunction):
     name = 'ST_Collect'
     type = Geometry
+
+
+def point_intersects_geom(point, geom, buffer=0.0):
+    """
+    return true or false whether point is in / out of the geom
+    """
+    ret_val = False
+    return ret_val
+
+
+def point_distance_to_geom(point, geom):
+    """
+    return true or false whether point is in / out of the geom
+    """
+    ret_val = False
+    return ret_val
