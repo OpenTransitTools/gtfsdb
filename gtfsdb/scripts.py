7ced3c182e5859f6a2c8da6faedc88818ff6474a
Frank Purcell <fxpurcell@gmail.com>
2020-12-29 15:51:28 -0800

    fix(cmdline): add handful of options to make more flexible for trfficdb

--- gtfsdb/scripts.py
+++ gtfsdb/scripts.py
@@ -7,7 +7,25 @@ from gtfsdb.model.base import Base
 from gtfsdb.api import database_load
 
 
-def get_args(prog_name='gtfsdb-load',):
+def make_kwargs(args):
+    # see below...
+    kwargs = dict(
+        # common cmd line items
+        url=args.database_url,
+        schema=args.schema,
+        is_geospatial=args.is_geospatial,
+        current_tables=args.current_tables,
+
+        # less used params
+        do_postprocess=not args.ignore_postprocess,
+        ignore_blocks=args.ignore_blocks,
+        tables=args.tables,
+        batch_size=args.batch_size
+    )
+    return kwargs
+
+
+def get_args(prog_name='gtfsdb-load', do_parse=True):
     """
     database load command-line arg parser and help util...
     """
@@ -28,27 +46,21 @@ def get_args(prog_name='gtfsdb-load',):
                         help='Database SCHEMA name')
     parser.add_argument('--tables', choices=tables, default=None, nargs='*',
                         help='Limited list of TABLES to load, if blank, load all tables')
+    parser.add_argument('--create', '-c', action="store_true",
+                        help='create new db tables (note: not currently used in gtfsdb, which always creates tables)')
     parser.add_argument('--current_tables', '-ct', default=False, action='store_true',
                         help="create tables that represent 'current' service (e.g., views)")
     parser.add_argument('--ignore_postprocess', '-np', default=False, action='store_true',
                         help="don't run any postprocess model routines (will leave some tables empty ... but will load raw gtfs data)")
     parser.add_argument('--ignore_blocks', '-nb', default=False, action='store_true',
                         help="don't bother populating the derrived block table")
-    args = parser.parse_args()
+    if do_parse:
+        args = parser.parse_args()
+        kwargs = make_kwargs(args)
+    else:
+        args = parser
+        kwargs = None
 
-    kwargs = dict(
-        # common cmd line items
-        url=args.database_url,
-        schema=args.schema,
-        is_geospatial=args.is_geospatial,
-        current_tables=args.current_tables,
-
-        # less used params
-        do_postprocess=not args.ignore_postprocess,
-        ignore_blocks=args.ignore_blocks,
-        tables=args.tables,
-        batch_size=args.batch_size
-    )
     return args, kwargs
 
 
