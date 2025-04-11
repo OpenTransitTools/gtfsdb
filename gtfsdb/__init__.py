8b752de98c27b1b5339430c16d73fe5352e0a856
Frank Purcell <fxpurcell@gmail.com>
2025-04-10 20:30:56 -0700

    feat: flex regions kinda working / populating

--- gtfsdb/__init__.py
+++ gtfsdb/__init__.py
@@ -55,5 +55,5 @@ CURRENT_CLASS_NAMES = [
     CurrentRoutes.__name__,
     CurrentRouteStops.__name__,
     CurrentStops.__name__,
-    LocationCarto.__name__,
+    FlexRegion.__name__,
 ]
--- gtfsdb/model/location.py
+++ gtfsdb/model/location.py
@@ -1,9 +1,10 @@
 import json
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import deferred
 
 from gtfsdb import config
+from gtfsdb.util import do_sql
 from gtfsdb.model.base import Base
 
 import logging
@@ -11,12 +12,10 @@ log = logging.getLogger(__name__)
 
 
 class LocationBase(object):
-    id = Column(String(255), primary_key=True, index=True, nullable=False)
-    region_color = Column(String(7), default=config.default_route_color)
-    text_color = Column(String(7), default=config.default_text_color)
     route_id = Column(String(255))
     region_name = Column(String(255))
-    region_desc = Column(String(1023))
+    region_color = Column(String(7), default=config.default_route_color)
+    text_color = Column(String(7), default=config.default_text_color)
 
     @classmethod
     def add_geometry_column(cls):
@@ -32,6 +31,8 @@ class Location(Base, LocationBase):
     filename = 'locations.geojson'
 
     __tablename__ = 'locations'
+
+    id = Column(String(255), primary_key=True, index=True, nullable=False)
 
     @classmethod
     def make_record(cls, row, **kwargs):
@@ -58,28 +59,39 @@ class Location(Base, LocationBase):
                     if stop_time:
                         #import pdb; pdb.set_trace()
                         l.route_id = stop_time.trip.route.route_id
+                        l.region_name = stop_time.trip.route.route_name
                         l.region_color = stop_time.trip.route.route_color
                         l.text_color = stop_time.trip.route.route_text_color
-                        l.region_name = stop_time.trip.route.route_name
         except Exception as e:
             log.error(e)
 
 
-class LocationCarto(Base, LocationBase):
+class FlexRegion(Base, LocationBase):
     """ a union of GTFS related (via stop_time.location_id) flex regions, that should look good on a map """
     datasource = config.DATASOURCE_DERIVED
-    __tablename__ = 'locations_carto'
+    __tablename__ = 'flex_region'
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
 
     @classmethod
     def post_process(cls, db, **kwargs):
         """
-        TODO: below query unions all regions for an agency ... need to union by route instead
+        query the location table, which unions all regions for the agency (group) by the route and other ids
 
-        drop table if exists sam.flex;
-        CREATE TABLE sam.flex AS
-        SELECT ST_UnaryUnion(ST_CollectionExtract(unnest(ST_ClusterIntersecting(geom)))) as geom
-        FROM sam.locations;
-        ALTER TABLE sam.flex ADD COLUMN id BIGSERIAL PRIMARY KEY;
+        --drop table if exists sam.flex_region;
+        --UPDATE TABLE sam.flex_region
+        insert into sam.flex_region (route_id, region_name, region_color, text_color, geom)
+        SELECT route_id, region_name, region_color, text_color, 
+               ST_UnaryUnion(ST_CollectionExtract(unnest(ST_ClusterIntersecting(geom)))) as geom
+        FROM sam.locations
+        group by route_id, region_name, region_color, text_color;
         """
         #import pdb; pdb.set_trace()
-        pass
+        schema = kwargs.get('schema', 'public')
+        sql = "" \
+        "insert into {}.flex_region (route_id, region_name, region_color, text_color, geom) " \
+        "SELECT route_id, region_name, region_color, text_color, " \
+        "ST_UnaryUnion(ST_CollectionExtract(unnest(ST_ClusterIntersecting(geom)))) as geom " \
+        "FROM {}.locations " \
+        "group by route_id, region_name, region_color, text_color".format(schema, schema)
+        do_sql(db, sql)
