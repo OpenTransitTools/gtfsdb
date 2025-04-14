f81e062cb0a56e3bc952b222a6500edb866a0ab3
Frank Purcell <fxpurcell@gmail.com>
2025-04-14 16:34:23 -0700

    fix: add separate rendering sort order

--- gtfsdb/model/location.py
+++ gtfsdb/model/location.py
@@ -10,10 +10,13 @@ from gtfsdb.model.base import Base
 import logging
 log = logging.getLogger(__name__)
 
+RSO_MAX=11111
+
 
 class LocationBase(object):
     route_id = Column(String(255))
     route_sort_order = Column(Integer, index=True)
+    render_order = Column(Integer, default=1)
     region_name = Column(String(255))
     region_color = Column(String(7), default=config.default_route_color)
     text_color = Column(String(7), default=config.default_text_color)
@@ -63,6 +66,7 @@ class Location(Base, LocationBase):
                         rso[l.route_id] = rso.get(l.route_id) or i  # for the 'default' route sort order below
                         l.route_id = stop_time.trip.route.route_id
                         l.route_sort_order = stop_time.trip.route.route_sort_order or rso.get(l.route_id) or 1
+                        l.render_order = RSO_MAX - l.route_sort_order
                         l.region_name = stop_time.trip.route.route_name
                         l.region_color = stop_time.trip.route.route_color
                         l.text_color = stop_time.trip.route.route_text_color
@@ -85,21 +89,21 @@ class FlexRegion(Base, LocationBase):
     def post_process(cls, db, **kwargs):
         """
         query the location table, which unions all regions for the agency (group) by the route and other ids
-
-        --drop table if exists sam.flex_region;
-        --UPDATE TABLE sam.flex_region
-        insert into sam.flex_region (route_id, route_sort_order, region_name, region_color, text_color, geom)
-        SELECT route_id, region_name, region_color, text_color, 
-               ST_UnaryUnion(ST_CollectionExtract(unnest(ST_ClusterIntersecting(geom)))) as geom
-        FROM sam.locations
-        group by route_id, region_name, region_color, text_color;
+        example sql:
+            --drop table if exists sam.flex_region;
+            --UPDATE TABLE sam.flex_region
+            insert into sam.flex_region (route_id, route_sort_order, render_order, region_name, region_color, text_color, geom)
+            SELECT route_id, region_name, region_color, text_color, 
+                    ST_UnaryUnion(ST_CollectionExtract(unnest(ST_ClusterIntersecting(geom)))) as geom
+            FROM sam.locations
+            group by route_id, region_name, region_color, text_color;
         """
-        route_columns = "route_id, route_sort_order, region_name, region_color, text_color"
+        #import pdb; pdb.set_trace()
+        route_columns = "route_id, route_sort_order, render_order, region_name, region_color, text_color"
         schema = kwargs.get('schema', 'public')
         sql = "" \
         "INSERT INTO {schema}.flex_region ({route_columns}, geom) " \
         "SELECT {route_columns}, ST_UnaryUnion(ST_CollectionExtract(unnest(ST_ClusterIntersecting(geom)))) as geom " \
         "FROM {schema}.locations " \
         "GROUP BY {route_columns}".format(schema=schema, route_columns=route_columns)
-        #import pdb; pdb.set_trace()
         do_sql(db, sql)
